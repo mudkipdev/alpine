c6b351c8f2a6a7b5a494ba4b63c90c706d558312
mudkipdev <mudkip@mudkip.dev>
2025-02-25 10:25:53 -0700

    separate binary module

---
+++ binary/build.gradle.kts
@@ -0,0 +1,3 @@
+dependencies {
+    compileOnly("io.netty:netty-buffer:4.1.118.Final")
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,32 +3,28 @@ plugins {
     `maven-publish`
 }
 
-group = "dev.mudkip"
-version = "0.1.0"
-
-java {
-    toolchain.languageVersion = JavaLanguageVersion.of(21)
-    withSourcesJar()
-    withJavadocJar()
-}
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    compileOnly("org.jetbrains:annotations:26.0.2")
-    compileOnly("io.netty:netty-buffer:4.1.118.Final")
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("library") {
-            from(components["java"])
-        }
-    }
+allprojects {
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
 
     repositories {
-        mavenLocal()
+        mavenCentral()
+    }
+
+    dependencies {
+        implementation(rootProject)
+        compileOnly("org.jetbrains:annotations:26.0.2")
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("library") {
+                from(components["java"])
+            }
+        }
+
+        repositories {
+            mavenLocal()
+        }
     }
 }
