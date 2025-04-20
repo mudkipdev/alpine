22205e7900939ea93a69e79e1f038d3f519eb0ea
mudkipdev <mudkip@mudkip.dev>
2025-04-19 18:12:00 -0600

    add built-in json codecs

--- json/src/main/java/alpine/json/JsonCodec.java
+++ json/src/main/java/alpine/json/JsonCodec.java
@@ -1,13 +1,29 @@
 package alpine.json;
 
-import alpine.json.Element;
-
 import java.util.function.Function;
 
 public interface JsonCodec<T, E extends Element> {
+    JsonCodec<Boolean, BooleanElement> BOOLEAN = of(BooleanElement::value, Element::bool);
+    JsonCodec<Number, NumberElement> NUMBER = of(NumberElement::value, Element::number);
+    JsonCodec<String, StringElement> STRING = of(StringElement::value, Element::string);
+
     T decode(E element);
 
     E encode(T value);
+
+    default <U> JsonCodec<U, E> map(Function<T, U> to, Function<U, T> from) {
+        return new JsonCodec<>() {
+            @Override
+            public U decode(E element) {
+                return to.apply(JsonCodec.this.decode(element));
+            }
+
+            @Override
+            public E encode(U value) {
+                return JsonCodec.this.encode(from.apply(value));
+            }
+        };
+    }
 
     static <T, E extends Element> JsonCodec<T, E> of(Function<E, T> decoder, Function<T, E> encoder) {
         return new JsonCodec<>() {
@@ -33,5 +49,9 @@ public interface JsonCodec<T, E extends Element> {
         return of(value -> {
             throw new UnsupportedOperationException("This JSON codec does not support encoding!");
         }, encoder);
+    }
+
+    static <T extends Enum<T>> JsonCodec<T, StringElement> name(Class<T> clazz) {
+        return STRING.map(name -> Enum.valueOf(clazz, name), Enum::name);
     }
 }
